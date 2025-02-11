8efffebab63785eb12cb4dabca21e8296822d3ce
Yash Singh <myemail>
2025-02-12 02:40:11 +0530

    full functional

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,6 +25,6 @@ io.on('connection', (socket: Socket) => {
   })
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(3001, () => {
+    console.log('listening on *:3001');
 });
